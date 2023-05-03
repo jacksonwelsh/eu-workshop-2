55d07956a90a92c75938ddd26547ef8239c91064
Andy <37462713+andym1125@users.noreply.github.com>
2023-05-02 19:39:37 -0500

    More homely link

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -17,7 +17,7 @@
 		</svg>
 		<ul>
 			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
-				<a href="/">My Home</a>
+				<a href="/">My Home Sweet Home</a>
 			</li>
 			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
 				<a href="/about">About Me</a>
